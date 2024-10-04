1ba74b28463e1ed09ef67de8b48a41177e07710a
SemakulaSean <seansemakulamuk@gmail.com>
2024-10-04 21:41:04 +0300

    Add files via upload

---
+++ Oopassignment.java
@@ -0,0 +1,24 @@
+package oopassignment;
+
+public class Oopassignment {
+
+	public static void main(String[] args) {
+	
+		for (int i= 2; i<=6; i++) {
+		System.out.println(i);
+	}
+System.out.println();
+int count=1;
+do {
+	System.out.println("Count" + count);
+			count++;
+}while (count<=5);
+System.out.println();
+int w=2;
+while (w<=6) {
+	System.out.println(w);
+	w++;
+}
+	}
+}
+
